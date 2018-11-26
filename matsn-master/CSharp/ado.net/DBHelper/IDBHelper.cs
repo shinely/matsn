130862f44b8a9ae370cac70a19307bff4405719d
matsn <meiqingqing8@qq.com>
2018-11-26 17:26:40 +0800

    ado.net DBHelper v1.0

---
+++ matsn-master/CSharp/ado.net/DBHelper/IDBHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper
+{
+    interface IDBHelper
+    {
+        /// <summary>
+        /// 自定义执行sql操作
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql"></param>
+        /// <param name="func"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        T Execute<T>(String sql, Func<DbCommand, T> func, params DbParameter[] parameters);
+
+        /// <summary>
+        /// 执行增删改的sql操作
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        int ExecuteNonQuery(String sql, params DbParameter[] parameters);
+
+        /// <summary>
+        /// 执行返回只有一个结果的sql操作
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        object ExecuteScalar(String sql, params DbParameter[] parameters);
+
+        /// <summary>
+        /// 执行查询操作，返回一个可读的DataReader
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        DbDataReader ExecuteReader(String sql, params DbParameter[] parameters);
+    }
+}
---
+++ matsn-master/CSharp/ado.net/DBHelper/Impl/MySqlHelper.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper
+{
+    public class MySqlHelper : IDBHelper
+    {
+        private static String connStr = ConfigurationManager.ConnectionStrings["mysql"].ToString();
+
+        private MySqlConnection GetConnection()
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            return conn;
+        }
+
+        public T Execute<T>(string sql, Func<DbCommand, T> func, params DbParameter[] parameters)
+        {
+            using (MySqlConnection conn = GetConnection())
+            using (MySqlCommand cmd = conn.CreateCommand())
+            {
+                if(parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                return func.Invoke(cmd);
+            }
+        }
+
+        public int ExecuteNonQuery(string sql, params DbParameter[] parameters)
+        {
+            return Execute(sql, cmd => cmd.ExecuteNonQuery(), parameters);
+        }
+
+        public DbDataReader ExecuteReader(string sql, params DbParameter[] parameters)
+        {
+            return Execute(sql, cmd => cmd.ExecuteReader(), parameters);
+        }
+
+        public object ExecuteScalar(string sql, params DbParameter[] parameters)
+        {
+            return Execute(sql, cmd => cmd.ExecuteScalar(), parameters);
+        }
+    }
+}
