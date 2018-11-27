76a2226ebc4739570197258686d4830e1223408a
matsn <meiqingqing8@qq.com>
2018-11-27 09:19:49 +0800

    ado.net v0.1

---
+++ asp.net/ado.net/DBHelper/DBHelper/Impl/SqlServerHelper.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper
+{
+    public class SqlServerHelper : IDBHelper
+    {
+        private static String connStr = ConfigurationManager.ConnectionStrings["sqlserver"].ToString();
+
+        private SqlConnection GetConnection()
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            return conn;
+        }
+
+        public T Execute<T>(string sql, Func<DbCommand, T> func, params DbParameter[] parameters)
+        {
+            using (SqlConnection conn = GetConnection())
+            using (SqlCommand cmd = conn.CreateCommand())
+            {
+                if(parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                return func.Invoke(cmd);
+            }
+        }
+
+        public int ExecuteNonQuery(string sql, params DbParameter[] parameters)
+        {
+            return Execute(sql, cmd => cmd.ExecuteNonQuery(), parameters);
+        }
+
+        public DbDataReader ExecuteReader(string sql, params DbParameter[] parameters)
+        {
+            return Execute(sql, cmd => cmd.ExecuteReader(), parameters);
+        }
+
+        public object ExecuteScalar(string sql, params DbParameter[] parameters)
+        {
+            return Execute(sql, cmd => cmd.ExecuteScalar(), parameters);
+        }
+    }
+}
---
+++ asp.net/ado.net/DBHelper/Factory/DBHelperFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelper.Factory
+{
+    class DBHelperFactory
+    {
+        // 配置文件参考以下配置
+        //<appSettings>
+        //  <add key = "IDBHelper" value="Ruanmou.DB.MySql.MySqlHelper,Ruanmou.DB.MySql"/>
+        //</appSettings>
+
+        private static String TypeDll = ConfigurationManager.AppSettings["IDBHelper"];
+        private static String DllName = TypeDll.Split(',')[1];
+        private static String TypeName = TypeDll.Split(',')[0];
+
+        public static IDBHelper CreateInstace()
+        {
+            Assembly assembly = Assembly.Load(DllName);
+            Type typeDBHelper = assembly.GetType(TypeName);
+            IDBHelper dBHelper = Activator.CreateInstance(typeDBHelper) as IDBHelper;
+            return dBHelper;            
+        }
+
+
+
+    }
+}
